0b5232f75cd3be790292f57de7fdb33e4c033640
Moksh Tehlan <tehlanmoksh@gmail.com>
2023-07-20 10:59:10 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        function1();
+    }
+
+    static void function1(){
+        System.out.println("Created this first function");
+    }
+}
