c7e9d9a8249086ef7f658e3c2fc3eea2b340f9ac
Moksh Tehlan <tehlanmoksh@gmail.com>
2023-07-20 11:03:52 +0530

    vikas commit

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,6 @@ public class Main {
 
     static void vikasFunction(){
         System.out.println("Created this function by vikas");
+        System.out.println("Created another change");
     }
 }
